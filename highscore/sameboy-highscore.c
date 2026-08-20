7ff94f3f8030ba3f60ace4f44ac504760d80dd06
Alice Mikhaylenko <alicem@gnome.org>
2026-08-20 11:31:56 +0400

    higshcore: Report printing margins

--- highscore/sameboy-highscore.c
+++ highscore/sameboy-highscore.c
@@ -252,7 +252,7 @@ print_image_cb (GB_gameboy_t *gb,
 
   GBytes *bytes = g_bytes_new_take (data, width * height);
 
-  hs_game_boy_core_emit_print_started (HS_GAME_BOY_CORE (self), bytes, width, height);
+  hs_game_boy_core_emit_print_started (HS_GAME_BOY_CORE (self), bytes, top_margin, bottom_margin);
 
   g_bytes_unref (bytes);
 }
